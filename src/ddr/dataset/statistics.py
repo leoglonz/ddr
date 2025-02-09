ed916308953df608029da6ef5bcf0f2f2544e241
Tadd Bindas <tkb5476@psu.edu>
2025-02-09 12:51:35 -0600

    added statistics reads/writes

--- src/ddr/dataset/statistics.py
+++ src/ddr/dataset/statistics.py
@@ -24,26 +24,25 @@ def set_statistics(cfg) -> pd.DataFrame:
     """
     attributes_name = "hydrofabric_v2.2"
     statistics_path = Path(cfg.data_sources.statistics)
-    statistics_path.mkdir(exists=True)
+    statistics_path.mkdir(exist_ok=True)
     stats_file = statistics_path / f"attribute_statistics_{attributes_name}.json"
     if stats_file.exists():
         log.info(f"Reading Attribute Statistics from file: {stats_file.name}")
         df = pd.read_csv(str(stats_file))
+        # TODO improve the logic for saving/selecting statistics
     else:
+        print("Reading CONUS hydrofabric to construct attribute statistics")
         gdf = gpd.read_file(cfg.data_sources.conus_hydrofabric, layer="divide-attributes")
-        data = []
-        for attribute in cfg.params.attributes:
-            data.append(gdf[attribute].values)
-        
         json_ = {}
-        for i, attr in enumerate(cfg.params.attributes):
+        for attr in cfg.params.attributes:
+            data = gdf[attr].values
             json_[attr] = {
-                "min": np.min(data, axis=0)[i],
-                "max": np.max(data, axis=0)[i],
-                "mean": np.mean(data, axis=0)[i],
-                "std": np.std(data, axis=0)[i],
-                "p10": np.percentile(data, 10, axis=0)[i],
-                "p90": np.percentile(data, 90, axis=0)[i],
+                "min": np.min(data, axis=0),
+                "max": np.max(data, axis=0),
+                "mean": np.mean(data, axis=0),
+                "std": np.std(data, axis=0),
+                "p10": np.percentile(data, 10, axis=0),
+                "p90": np.percentile(data, 90, axis=0),
             }
         df = pd.DataFrame(json_).T
         df.to_csv(str(stats_file))
